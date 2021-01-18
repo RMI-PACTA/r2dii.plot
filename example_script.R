262f3bb2ad59cdc01549552c53cb7a9deaf5abfc
MonikaFu <monika.furdyna@gmail.com>
2021-01-18 18:44:37 +0100

    Modify example_script

--- example_script.R
+++ example_script.R
@@ -12,15 +12,15 @@ scenario_specs <- data.frame(scenario = c("sds","sps","cps","worse"),
                              color = c("#9CAB7C","#FFFFCC","#FDE291","#E07B73"),
                              label = c("SDS","STEPS","CPS","worse"))
 
-mainLineMetric <- data.frame(metric = "projected", label = "Portfolio")
-additionalLineMetrics <- data.frame(metric = "corporate_economy", label = "Corporate Economy")
+main_line_metric <- data.frame(metric = "projected", label = "Portfolio")
+additional_line_metrics <- data.frame(metric = "corporate_economy", label = "Corporate Economy")
 
 plot <- plot_trajectory_chart(data_trajectory,
-                              plotTitle = "Production trajectory of Oil Capacity technology \n in the Power sector",
-                              xTitle = "Year", yTitle = "Production rate (normalized to 2020)",
-                              annotateData = FALSE,
+                              plot_title = "Production trajectory of Oil Capacity technology \n in the Power sector",
+                              x_title = "Year", y_title = "Production rate (normalized to 2020)",
+                              annotate_data = FALSE,
                               scenario_specs_good_to_bad = scenario_specs,
-                              mainLineMetric, additionalLineMetrics)
+                              main_line_metric, additional_line_metrics)
 
 
 plot
@@ -37,7 +37,7 @@ bars_labels_specs <- data.frame("metric_type" = c("portfolio_2020", "benchmark_2
                  "label" = c("Portfolio 2020", "Benchmark 2020", "Portfolio 2025", "Benchmark 2025", "Target SDS 2025"))
 
 plot_techmix_power <- plot_techmix_chart(data_techmix_power,"Technology mix for the Power sector",
-                                         showLegend = TRUE, tech_colors_power, bars_labels_specs)
+                                         show_legend = TRUE, tech_colors_power, bars_labels_specs)
 plot_techmix_power
 
 data_techmix_auto <- filter_data_for_techmix_chart(example_data,sector = "automotive",
@@ -47,6 +47,8 @@ data_techmix_auto <- filter_data_for_techmix_chart(example_data,sector = "automo
 
 tech_colors_auto <- get_sector_colors("automotive")
 
-plot_techmix_auto <- plot_techmix_chart(data_techmix_auto,"Technology mix for the Automotive sector", showLegend = TRUE,
+plot_techmix_auto <- plot_techmix_chart(data_techmix_auto,
+                                        "Technology mix for the Automotive sector",
+                                        show_legend = TRUE,
                                    tech_colors_auto, bars_labels_specs)
 plot_techmix_auto
