6b3ba97ae99c275172c550d3b1ee5895d078dc64
MonikaFu <monika.furdyna@gmail.com>
2021-01-26 15:42:47 +0100

    Document data function

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,4 @@
 #' @import ggplot2
 #' @importFrom dplyr arrange case_when filter group_by if_else left_join mutate
-#' @importFrom dplyr select tribble
+#' @importFrom dplyr select tribble summarise ungroup
 NULL
