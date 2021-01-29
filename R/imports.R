3b3df3dbc0e66078f1c51768bf4e7b1783941014
MonikaFu <monika.furdyna@gmail.com>
2021-01-29 11:46:10 +0100

    Add ggpubr functions to import

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,5 @@
 #' @import ggplot2
 #' @importFrom dplyr arrange case_when filter group_by if_else left_join mutate
 #' @importFrom dplyr select tribble summarise ungroup pull
+#' @importFrom ggpubr ggarrange annotate_figure text_grob
 NULL
