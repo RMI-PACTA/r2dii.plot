de47062f4ae388ff761f6aaaa706b59b4de8e2ea
Mauro Lepore <maurolepore@gmail.com>
2021-02-12 13:43:45 -0600

    get_example_data() has expected type of columns

--- tests/testthat/test-get_example_data.R
+++ tests/testthat/test-get_example_data.R
@@ -1,3 +1,22 @@
 test_that("outputs a data.frame", {
   expect_s3_class(get_example_data(), "data.frame")
 })
+
+test_that("has the expected type of columns", {
+  expected <- c(
+      metric = "character",
+      production = "double",
+      region = "character",
+      scenario_source = "character",
+      sector = "character",
+      technology = "character",
+      technology_share = "double",
+      year = "double"
+    )
+
+  data <- get_example_data()
+  sorted <- data[sort(names(data))]
+  actual <- vapply(sorted, typeof, character(1))
+
+  expect_equal(actual, expected)
+})
