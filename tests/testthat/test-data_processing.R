ffa268c007a163ffcdf073b52264f7909f2be9a7
Mauro Lepore <maurolepore@gmail.com>
2021-02-12 16:53:24 -0600

    Make tests use smaller data

--- tests/testthat/test-data_processing.R
+++ tests/testthat/test-data_processing.R
@@ -1,20 +1,15 @@
 test_that("inputs a data frame structured as get_example_data()", {
-  raw <- get_example_data()
-
   no_error <- NA
-  expect_error(process_input_data(raw), no_error)
+  expect_error(process_input_data(fake_data()), no_error)
 })
 
 test_that("outputs a data.frame, invisibly", {
-  raw <- get_example_data()
-
-  no_error <- NA
-  expect_invisible(process_input_data(raw))
-  expect_s3_class(process_input_data(raw), "data.frame")
+  expect_s3_class(process_input_data(fake_data()), "data.frame")
+  expect_invisible(process_input_data(fake_data()))
 })
 
 test_that("adds a column `metric_type`", {
-  before <- get_example_data()
+  before <- fake_data()
   expect_false(hasName(before, "metric_type"))
 
   after <- process_input_data(before)
@@ -25,7 +20,7 @@ test_that("adds a column `metric_type`", {
 # too easily become obsolete when the implementation changes. Instead you may
 # better "document" this expectation with an informative error message.
 test_that("depends on input column `metric`", {
-  bad <- get_example_data()
+  bad <- fake_data()
   bad$metric <- NULL
 
   # FIXME: The error message could be more graceful
