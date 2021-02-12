7562b804d1369b067f7b9d0be55af435647e2644
Mauro Lepore <maurolepore@gmail.com>
2021-02-12 16:43:04 -0600

    Test some documented expectations

--- tests/testthat/test-data_processing.R
+++ tests/testthat/test-data_processing.R
@@ -20,3 +20,21 @@ test_that("adds a column `metric_type`", {
   after <- process_input_data(before)
   expect_true(hasName(after, "metric_type"))
 })
+
+# FIXME: The helpfile shoud likely not expose implementation details. That will
+# too easily become obsolete when the implementation changes. Instead you may
+# better "document" this expectation with an informative error message.
+test_that("depends on input column `metric`", {
+  bad <- get_example_data()
+  bad$metric <- NULL
+
+  # FIXME: The error message could be more graceful
+  message <- "nms %in% .* are not all TRUE"
+  expect_error(process_input_data(bad))
+})
+
+test_that("modifies `metric`", {
+  before <- get_example_data()
+  after <- process_input_data(before)
+  expect_false(identical(before$metric, after$metric))
+})
