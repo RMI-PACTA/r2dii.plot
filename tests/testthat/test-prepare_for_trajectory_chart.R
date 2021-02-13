a2461de7b7f716c5a8b4bb5950e396cf9353e268
Mauro Lepore <maurolepore@gmail.com>
2021-02-12 18:57:55 -0600

    Test arguments

--- tests/testthat/test-prepare_for_trajectory_chart.R
+++ tests/testthat/test-prepare_for_trajectory_chart.R
@@ -31,7 +31,6 @@ test_that("by default outputs invisibly", {
 
 test_that("with `normalize_to_start_year = FALSE` outputs visibly", {
   dont_normalize <- FALSE
-
   data <- process_input_data(get_example_data())
 
   expect_visible(
@@ -47,3 +46,135 @@ test_that("with `normalize_to_start_year = FALSE` outputs visibly", {
   )
 })
 
+# FIXME: We could throw a more graceful warning.
+test_that("with bad `sector_filter` warns ungracefully", {
+  data <- process_input_data(get_example_data())
+  bad <- "bad"
+  ungraceful_message <- "no non-missing arguments to min"
+
+  expect_warning(
+    prepare_for_trajectory_chart(
+      data,
+      sector_filter = bad,
+      technology_filter = "oilcap",
+      region_filter = "global",
+      scenario_source_filter = "demo_2020",
+      value_name = "production"
+    ),
+    ungraceful_message
+  )
+})
+
+# FIXME: We could throw a more graceful warning.
+test_that("with bad `technology_filter` warns ungracefully", {
+  data <- process_input_data(get_example_data())
+  bad <- "bad"
+  ungraceful_message <- "no non-missing arguments to min"
+
+  expect_warning(
+    prepare_for_trajectory_chart(
+      data,
+      sector_filter = "power",
+      technology_filter = bad,
+      region_filter = "global",
+      scenario_source_filter = "demo_2020",
+      value_name = "production"
+    ),
+    ungraceful_message
+  )
+})
+
+# FIXME: We could throw a more graceful warning.
+test_that("with bad `region_filter` warns ungracefully", {
+  data <- process_input_data(get_example_data())
+  bad <- "bad"
+  ungraceful_message <- "no non-missing arguments to min"
+
+  expect_warning(
+    prepare_for_trajectory_chart(
+      data,
+      sector_filter = "power",
+      technology_filter = "oilcap",
+      region_filter = bad,
+      scenario_source_filter = "demo_2020",
+      value_name = "production"
+    ),
+    ungraceful_message
+  )
+})
+
+# FIXME: We could throw a more graceful warning.
+test_that("with bad `scenario_source_filter` warns ungracefully", {
+  data <- process_input_data(get_example_data())
+  bad <- "bad"
+  ungraceful_message <- "no non-missing arguments to min"
+
+  expect_warning(
+    prepare_for_trajectory_chart(
+      data,
+      sector_filter = "power",
+      technology_filter = "oilcap",
+      region_filter = "global",
+      scenario_source_filter = bad,
+      value_name = "production"
+    ),
+    ungraceful_message
+  )
+})
+
+# FIXME: We could throw a more graceful error.
+test_that("with bad `value_name` errors ungracefully", {
+  data <- process_input_data(get_example_data())
+  bad <- "bad"
+  ungraceful_message <- "Can't subset columns that don't exist"
+
+  expect_error(
+    prepare_for_trajectory_chart(
+      data,
+      sector_filter = "power",
+      technology_filter = "oilcap",
+      region_filter = "global",
+      scenario_source_filter = "demo_2020",
+      value_name = bad
+    ),
+    ungraceful_message
+  )
+})
+
+# FIXME: This may need an error or warning
+test_that("with bad `end_year_filter` throws no error", {
+  data <- process_input_data(get_example_data())
+  bad <- "bad"
+  no_error <- NA
+
+  expect_error(
+    prepare_for_trajectory_chart(
+      data,
+      sector_filter = "power",
+      technology_filter = "oilcap",
+      region_filter = "global",
+      scenario_source_filter = "demo_2020",
+      value_name = "production",
+      end_year_filter = bad
+    ),
+    no_error
+  )
+})
+
+test_that("with bad `normalize_to_start_year` errors gracefully", {
+  data <- process_input_data(get_example_data())
+  bad <- "bad"
+
+  expect_error(
+    prepare_for_trajectory_chart(
+      data,
+      sector_filter = "power",
+      technology_filter = "oilcap",
+      region_filter = "global",
+      scenario_source_filter = "demo_2020",
+      value_name = "production",
+      normalize_to_start_year = bad
+    ),
+    "not.*logical"
+  )
+})
