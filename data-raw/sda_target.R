5eca0352ec75e672cc67e249769f1bf0b999777a
MonikaFu <monika.furdyna@gmail.com>
2021-02-24 17:41:11 +0100

    Add the data set to data folder and ignore data-raw

--- data-raw/sda_target.R
+++ data-raw/sda_target.R
@@ -1,3 +1,3 @@
 # Source: @MonikaFu https://github.com/2DegreesInvesting/r2dii.ggplot/issues/55
-sda_data <- readr::read_csv(here::here("data-raw", "sda_target.csv"))
-use_data(sda_data, overwrite = TRUE)
+sda_target <- readr::read_csv(here::here("data-raw", "sda_target.csv"))
+usethis::use_data(sda_target, overwrite = TRUE)
