34c504fa692951bea363529ac04bde4bc6f3cc1d
MonikaFu <monika.furdyna@gmail.com>
2021-02-26 15:13:49 +0100

    Update documentation

--- R/sda_target.R
+++ R/sda_target.R
@@ -3,15 +3,15 @@
 #' An example data set containing emission factors for different metric types:
 #' projected, benchmark and scenarios and different sectors.
 #'
-#' @format A data frame with 208 rows and 4 variables:
-#' \describe{
-#'   \item{sector}{industry sector (as defined in PACTA)}
-#'   \item{year}{year for which emissions are reported}
-#'   \item{emission_factor_metric}{metric type: 'projected' for the test
-#'   portfolio 'corporate_economy' for the benchmark, 'adjusted_scenario_demo'
-#'   for a scenario}
-#'   \item{emission_factor_value}{emissions value, a double, units differ
-#'   depending on the sector}
-#' }
-#' @source \url{https://github.com/2DegreesInvesting/r2dii.ggplot/issues/55}
+#' Column definitions:
+#' * `sector`: Industry sector (as defined in PACTA).
+#' * `year`: Year for which emissions are reported.
+#' * `emission_factor_metric`: metric type: 'projected' for the test portfolio
+#' 'corporate_economy' for the benchmark, 'adjusted_scenario_demo' for a scenario.
+#' * `emission_factor_value`: emissions value, a double, units differ depending
+#' on the sector.
+#'
+#' @source <https://github.com/2DegreesInvesting/r2dii.ggplot/issues/55>.
+#' @examples
+#' sda_target
 "sda_target"
