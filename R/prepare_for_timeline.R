5b0c7c245c4e27962d3d9482c23334f475ffb742
MonikaFu <monika.furdyna@gmail.com>
2021-03-03 17:53:15 +0100

    Add input checks

--- R/prepare_for_timeline.R
+++ R/prepare_for_timeline.R
@@ -5,8 +5,8 @@
 #'   (dataframe).
 #' @param sector_filter Sector to be used for filtering (character string or a
 #'   vector of character strings).
-#' @param year_start Start year of the plot (interger).
-#' @param year_end End year of the plot (interger).
+#' @param year_start Start year of the plot (double).
+#' @param year_end End year of the plot (double).
 #' @param column_line_names Column specifying the names of lines to be plotted
 #'   (character string).
 #' @param value_to_plot Column name of the value to be plotted (character
@@ -34,8 +34,26 @@ prepare_for_timeline <- function(sda_target_data,
                                  column_line_names = "emission_factor_metric",
                                  value_to_plot = "emission_factor_value") {
 
+  # Check inputs
   sector_filter <- match.arg(sector_filter)
 
+  if (typeof(year_start) != "double") {
+    stop("'year_start' should be a number.")
+  }
+
+  if (typeof(year_start) != "double") {
+    stop("'year_end' should be a number.")
+  }
+
+  if (!(column_line_names %in% names(sda_target_data))) {
+    stop(paste0("'", column_line_names, "' not found as a column in the input data. Please pass an existing column name to 'column_line_names'."))
+  }
+
+  if (!(value_to_plot %in% names(sda_target_data))) {
+    stop(paste0("'", value_to_plot, "' not found as a column in the input data. Please pass an existing column name to 'value_to_plot'."))
+  }
+
+  # Create output data
   data_timeline <- sda_target_data %>%
     filter(.data$sector == !!sector_filter) %>%
     filter(.data$year >= year_start) %>%
