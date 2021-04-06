830de752278a569a2009f7420c0d20d5af0c5862
MonikaFu <monika.furdyna@gmail.com>
2021-04-06 15:05:07 +0200

    Filter out data starting before "projected"

--- R/prepare_for_trajectory_chart.R
+++ R/prepare_for_trajectory_chart.R
@@ -38,11 +38,17 @@ prepare_for_trajectory_chart <- function(data_preprocessed,
                                          value_name,
                                          end_year_filter = 2025,
                                          normalize_to_start_year = TRUE) {
+
+  year_start_projected <- min(data_preprocessed %>%
+                    filter(.data$metric == 'projected') %>%
+                      pull(.data$year))
+
   data_filtered <- data_preprocessed %>%
     filter(.data$sector == !!sector_filter) %>%
     filter(.data$technology == !!technology_filter) %>%
     filter(.data$region == !!region_filter) %>%
     filter(.data$scenario_source == !!scenario_source_filter) %>%
+    filter(.data$year >= year_start_projected) %>%
     filter(.data$year <= end_year_filter) %>%
     select(.data$year, .data$metric_type, .data$metric, .data$technology,
       value = !!value_name
