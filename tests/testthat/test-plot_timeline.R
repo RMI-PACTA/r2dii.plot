be7264460f4b7b4090839ff7c09208e80dcf4290
MonikaFu <monika.furdyna@gmail.com>
2021-04-06 12:02:24 +0200

    Update printing test

--- tests/testthat/test-plot_timeline.R
+++ tests/testthat/test-plot_timeline.R
@@ -30,7 +30,12 @@ test_that("prints output ggplot object without error", {
   )
 
   plot <- plot_timeline(data_sda_cement)
-  expect_error(print(plot), NA)
+  expect_error({
+    path <- tempfile(fileext = ".pdf")
+    pdf(path)
+    print(plot)
+    dev.off()
+    }, NA)
 })
 
 test_that("with bad 'lines_specs' errors gracefully", {
