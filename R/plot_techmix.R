9d90bfb0bb22e3c29ffb029ef15eb05e488d05c2
MonikaFu <monika.furdyna@gmail.com>
2021-04-12 18:15:02 +0200

    Separate important functions

---
+++ R/plot_techmix.R
@@ -0,0 +1,62 @@
+#' Create a techmix chart in a ggplot object
+#'
+#' The function returns a ggplot object containing a stacked bar chart showing a
+#' technology mix for different categories (portfolio, scenario, benchmark,
+#' etc.).
+#'
+#' @param data Filtered input data (dataframe with columns: technology,
+#'   metric_type, metric and value).
+#' @param plot_title Title of the plot (character string).
+#' @param show_legend Flag indicating whether legend should be shown (boolean).
+#' @param df_tech_colours Dataframe containing colors per technology (dataframe
+#'   with columns: technology, label, color).
+#' @param df_bar_specs Dataframe containing order of bars and their labels
+#'   (dataframe with columns: metric_type, label).
+#'
+#' @export
+#' @examples
+#' # TODO create an example or copy-paste an existing one from README or a test.
+plot_techmix <- function(data,
+                         plot_title = "",
+                         show_legend = TRUE,
+                         df_tech_colours,
+                         df_bar_specs) {
+  data_colours <- df_tech_colours %>%
+    filter(.data$technology %in% unique(!!data$technology))
+
+  data <- data %>%
+    filter(.data$metric_type %in% df_bar_specs$metric_type)
+
+  p_techmix <- ggplot() +
+    theme_2dii_ggplot() +
+    xlab("") +
+    ylab("") +
+    labs(title = plot_title)
+
+  p_techmix <- p_techmix +
+    geom_bar(data = data, aes(
+      fill = factor(.data$technology, levels = data_colours$technology),
+      x = factor(.data$metric_type, levels = rev(df_bar_specs$metric_type)),
+      y = .data$value
+    ), position = "fill", stat = "identity", width = .5) +
+    scale_y_continuous(
+      labels = scales::percent_format(), expand = c(0, 0),
+      sec.axis = dup_axis()
+    ) +
+    scale_x_discrete(labels = rev(df_bar_specs$label)) +
+    scale_fill_manual(labels = data_colours$label, values = data_colours$colour) +
+    coord_flip() +
+    theme(axis.line.y = element_blank()) +
+    theme(axis.ticks.y = element_blank())
+
+  if (show_legend) {
+    p_techmix <- p_techmix +
+      theme(legend.position = "bottom") +
+      guides(fill = guide_legend(ncol = 3, byrow = TRUE))
+  } else {
+    p_techmix <- p_techmix +
+      theme(legend.position = "none")
+  }
+
+  p_techmix
+}
---
+++ R/theme_2dii_ggplot.R
@@ -0,0 +1,42 @@
+#' Returns a custom 2dii ggplot theme
+#'
+#' Returns a ggplot theme which can be applied to all graphs to appear according
+#' to 2DII plotting aesthetics, like removed gridlines, grey axis lines etc.
+#'
+#' @param font_family argument specifying the font that should be used in a
+#'   graph (character string taking the same values as standard ggplot font
+#'   families).
+#'
+#' @export
+
+theme_2dii_ggplot <- function(font_family = "Helvetica") {
+  font_size_ticks <- 10
+  font_size_axis_titles <- 12
+  supporting_elts_color <- "#C0C0C0"
+
+  theme_classic() %+replace%
+    theme(
+      plot.margin = unit(c(0.5, 1, 0.5, 0.5), "cm"),
+      axis.line = element_line(colour = supporting_elts_color),
+      axis.ticks = element_line(colour = supporting_elts_color),
+      plot.title = element_text(
+        hjust = 0.5, vjust = 0.5, face = "bold",
+        family = font_family, size = 14,
+        margin = margin(20, 2, 12, 2)
+      ),
+      axis.text = element_text(
+        family = font_family, size = font_size_ticks,
+        margin = margin(5, 5, 5, 5)
+      ),
+      axis.title = element_text(
+        family = font_family,
+        size = font_size_axis_titles,
+        margin = margin(5, 5, 5, 5)
+      ),
+      legend.text = element_text(
+        family = font_family, size = 9,
+        margin = margin(5, 5, 5, 5)
+      ),
+      legend.title = element_blank()
+    )
+}
