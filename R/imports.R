4cea138f51f96a42638f316f8bc22946a782bf47
Mauro Lepore <maurolepore@gmail.com>
2021-04-14 10:12:30 -0500

    Remove needless dependency ggpubr

--- R/imports.R
+++ R/imports.R
@@ -2,8 +2,6 @@
 #' @importFrom dplyr arrange case_when filter group_by if_else left_join mutate
 #' @importFrom dplyr select tribble summarise ungroup pull distinct right_join
 #' @importFrom dplyr desc lead tibble
-#' @importFrom ggpubr ggarrange annotate_figure text_grob
-#' @importFrom ggpubr get_legend
 #' @importFrom maps iso.alpha
 #' @importFrom tidyr replace_na
 #' @importFrom stats na.omit
