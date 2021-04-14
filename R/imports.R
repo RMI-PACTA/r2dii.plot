e3be5b701b1ed97cc66c48c51955d780dfbaae54
Mauro Lepore <maurolepore@gmail.com>
2021-04-14 10:17:44 -0500

    Remove needless dependency maps

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,6 @@
 #' @importFrom dplyr arrange case_when filter group_by if_else left_join mutate
 #' @importFrom dplyr select tribble summarise ungroup pull distinct right_join
 #' @importFrom dplyr desc lead tibble
-#' @importFrom maps iso.alpha
 #' @importFrom tidyr replace_na
 #' @importFrom stats na.omit
 #' @importFrom rlang %||%
