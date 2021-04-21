58128686b4c8bac844e16a46ababa9a38e1f6f50
Mauro Lepore <maurolepore@gmail.com>
2021-04-21 14:24:15 -0500

    Import rlang's .env

--- R/imports.R
+++ R/imports.R
@@ -2,6 +2,7 @@
 #' @importFrom dplyr arrange case_when filter group_by if_else left_join mutate
 #' @importFrom dplyr select tribble summarise ungroup pull distinct right_join
 #' @importFrom dplyr desc lead tibble
+#' @importFrom rlang .env
 NULL
 
 utils::globalVariables("where")
