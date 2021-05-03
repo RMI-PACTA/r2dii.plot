ac7fab3b39756d09084e8757ba4d6dcf016356f3
MonikaFu <monika.furdyna@gmail.com>
2021-05-03 11:46:18 +0200

    Remove option to annotate

--- R/plot_trajectory.R
+++ R/plot_trajectory.R
@@ -8,8 +8,6 @@
 #'   metric and value).
 #' @param plot_title Title of the plot (character string).
 #' @param x_title,y_title Title of the x- and y-axis (character string).
-#' @param annotate_data Flag indicating whether the data should be annotated
-#'   (boolean).
 #' @param scenario_specs_good_to_bad Dataframe containing scenario
 #'   specifications like color or label, ordered from the most to least
 #'   sustainable (dataframe with columns: scenario, label, color).
@@ -25,7 +23,6 @@ plot_trajectory <- function(data,
                             plot_title = "",
                             x_title = "",
                             y_title = "",
-                            annotate_data = FALSE,
                             # FIXME: obligatory arguments come before optional
                             scenario_specs_good_to_bad,
                             # FIXME: obligatory arguments come before optional
@@ -37,15 +34,8 @@ plot_trajectory <- function(data,
     theme(axis.line = element_blank()) +
     xlab(x_title) +
     ylab(y_title) +
-    labs(title = plot_title)
-
-  if (annotate_data) {
-    p_trajectory <- p_trajectory +
-      theme(plot.margin = unit(c(0.5, 4, 0.5, 0.5), "cm"))
-  } else {
-    p_trajectory <- p_trajectory +
-      theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))
-  }
+    labs(title = plot_title) +
+    theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))
 
   # adjusting the area border to center the starting point of the lines
   lower_area_border <- min(data$value)
@@ -68,9 +58,11 @@ plot_trajectory <- function(data,
     if (perc_distance_upper_border > perc_distance_lower_border) {
       lower_area_border <-
         start_value_portfolio - perc_distance_upper_border * value_span
+      value_span <- upper_area_border - lower_area_border
     } else {
       upper_area_border <-
         perc_distance_lower_border * value_span + start_value_portfolio
+      value_span <- upper_area_border - lower_area_border
     }
   }
 
@@ -150,41 +142,15 @@ plot_trajectory <- function(data,
           )
       }
     }
-
-    if (annotate_data) {
-      last_year <- max(data$year)
-
-      p_trajectory <- p_trajectory +
-        annotate("segment",
-          x = last_year, xend = last_year + 0.75,
-          y = data_scen[data_scen$year == last_year, ]$value,
-          yend = data_scen[data_scen$year == last_year, ]$value,
-          colour = color
-        ) +
-        annotate("text",
-          x = (last_year + 0.85),
-          (y <- data_scen[data_scen$year == last_year, ]$value),
-          label = scenario_specs$label[i], hjust = 0, size = 3
-        )
-    }
   }
 
   data_mainline <- data %>% filter(.data$metric == main_line_metric$metric)
   p_trajectory <- p_trajectory +
     geom_line(
-      data = data_mainline, aes(x = year, y = .data$value),
+      data = data_mainline,
+      aes(x = year, y = .data$value),
       linetype = "solid"
     )
-
-  if (annotate_data) {
-    p_trajectory <- p_trajectory +
-      annotate("text",
-        x = (last_year + 0.1), (
-          y <- data_mainline[data_mainline$year == last_year, ]$value
-        ),
-        label = main_line_metric$label, hjust = 0, size = 3
-      )
-  }
 
   if (length(additional_line_metrics) >= 1) {
     linetypes_supporting <- c("dashed", "solid", "solid", "twodash")
@@ -200,15 +166,6 @@ plot_trajectory <- function(data,
           data = data_metric, aes(x = year, y = .data$value),
           linetype = linetype_metric, color = color_metric
         )
-
-      if (annotate_data) {
-        p_trajectory <- p_trajectory +
-          annotate("text",
-            x = (last_year + 0.1), (
-              y <- data_metric[data_metric$year == last_year, ]$value),
-            label = label_metric, hjust = 0, size = 3
-          )
-      }
     }
   }
   p_trajectory
