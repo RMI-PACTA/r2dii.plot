b9a8803b25723b091239d918fbb4ab213dae9d52
Mauro Lepore <maurolepore@gmail.com>
2021-05-21 15:35:05 -0600

    Test valid range of n

--- tests/testthat/test-plot_trajectory.R
+++ tests/testthat/test-plot_trajectory.R
@@ -2,3 +2,10 @@ test_that("get_ordered_scenario_colours errors with `n` outside valid range", {
   expect_snapshot_error(get_ordered_scenario_colours(1))
   expect_snapshot_error(get_ordered_scenario_colours(6))
 })
+
+test_that("get_ordered_scenario_colours works with `n` in valid range", {
+  expect_equal(nrow(get_ordered_scenario_colours(2)), 2)
+  expect_equal(nrow(get_ordered_scenario_colours(3)), 3)
+  expect_equal(nrow(get_ordered_scenario_colours(4)), 4)
+  expect_equal(nrow(get_ordered_scenario_colours(5)), 5)
+})
