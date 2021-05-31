9fe84459f8200f2330d2bdcf145c9d2496957f60
MonikaFu <monika.furdyna@gmail.com>
2021-05-31 13:21:11 +0200

    Remove legend and unnecessary helpers

--- R/plot_trajectory.R
+++ R/plot_trajectory.R
@@ -54,7 +54,7 @@ plot_trajectory <- function(data,
     scenario_specs_good_to_bad, data$technology[1]
   )
   data_scenarios <- get_scenario_data(data, scenario_specs)
-  colours_scenarios <- get_adjusted_colours(data_scenarios, scenario_specs)
+  colours_scenarios <- scenario_specs$colour
   p_trajectory <- ggplot() +
     geom_ribbon(
       data = data_scenarios,
@@ -169,17 +169,6 @@ plot_trajectory <- function(data,
     guides(linetype = FALSE, colour = FALSE)  # remove legend for "projected"
 
   p_trajectory
-  # legend <- plot_trajectory_legend(
-  #   p_trajectory,
-  #   data_scenarios,
-  #   scenario_specs,
-  #   data_metrics,
-  #   linetypes_ordered,
-  #   linecolours_ordered,
-  #   line_labels
-  # )
-  #
-  # add_grobs(p_trajectory, get_legend(legend))
 }
 
 check_number_scenarios <- function(scenario_specs) {
@@ -313,99 +302,4 @@ get_scenario_data <- function(data, scenario_specs) {
         default = area_borders$lower
       ))
   }
-}
-
-get_adjusted_colours <- function(data_scenarios,
-                                 scenario_specs) {
-  p_colors <- help_plot_area_colors(
-    data_scenarios,
-    scenario_specs
-  )
-
-  g <- ggplot_build(p_colors)
-  colors <- unique(g$plot$scales$scales[[1]]$palette.cache)
-
-  colors
-}
-
-plot_trajectory_legend <- function(plot,
-                                   data_scenarios,
-                                   scenario_specs,
-                                   data_metrics,
-                                   linetypes_ordered,
-                                   linecolours_ordered,
-                                   line_labels) {
-  p_legend <- help_plot_area_colors(data_scenarios, scenario_specs)
-
-  n_lines <- length(line_labels)
-  p_legend <- p_legend +
-    geom_line(
-      data = data_metrics,
-      aes(
-        x = .data$year,
-        y = .data$value,
-        linetype = .data$metric,
-        color = .data$metric
-      )
-    ) +
-    scale_linetype_manual(
-      values = linetypes_ordered[1:n_lines],
-      labels = line_labels
-    ) +
-    scale_color_manual(
-      values = linecolours_ordered[1:n_lines],
-      labels = line_labels
-    )
-
-  p_legend
-}
-
-help_plot_area_colors <- function(data_scenarios,
-                                  scenario_specs) {
-  lower_area_border <- min(data_scenarios$value)
-  upper_area_border <- max(data_scenarios$value)
-  num_scen <- nrow(scenario_specs)
-  value_span <- upper_area_border - lower_area_border
-
-  p_legend <- ggplot() +
-    theme_2dii() +
-    geom_point(
-      data = data_scenarios,
-      aes(
-        x = .data$year,
-        y = .data$value,
-        fill = .data$value
-      )
-    ) +
-    scale_fill_stepsn(
-      colours = scenario_specs$colour,
-      guide = "coloursteps",
-      breaks = seq(
-        from = lower_area_border + value_span / num_scen,
-        to = upper_area_border - value_span / num_scen,
-        by = value_span / num_scen
-      ),
-      labels = scenario_specs$label[scenario_specs$scenario != "worse"]
-    )
-
-  p_legend
-}
-
-add_grobs <- function(plot, legend) {
-  gridExtra::grid.arrange(
-    gridExtra::arrangeGrob(
-      plot + theme(legend.position = "none"),
-      nrow = 1
-    ),
-    legend,
-    ncol = 2,
-    widths = c(7, 3)
-  )
-}
-
-# https://stackoverflow.com/questions/13649473/add-a-common-legend-for-combined-ggplots
-get_legend <- function(plot) {
-  tmp <- ggplot_gtable(ggplot_build(plot))
-  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
-  tmp$grobs[[leg]]
 }
