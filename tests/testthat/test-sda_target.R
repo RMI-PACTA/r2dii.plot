d5e6616d5c36788d3ffebda8fa9acb27ff017009
Mauro Lepore <maurolepore@gmail.com>
2021-05-31 10:07:22 -0600

    Prune files

--- tests/testthat/test-sda_target.R
+++
@@ -1,9 +0,0 @@
-test_that("outputs a data.frame", {
-  expect_s3_class(sda, "data.frame")
-})
-
-test_that("outputs the expected snapshot", {
-  skip_if(r_version_is_older_than(4))
-
-  expect_snapshot(sda)
-})
