dfe13ae5c963fee7a34e57d8737573899ba3eb7d
Mauro Lepore <maurolepore@gmail.com>
2021-06-01 14:20:43 -0600

    Run tests faster

--- tests/testthat/test-prep_timeline.R
+++ tests/testthat/test-prep_timeline.R
@@ -1,43 +1,51 @@
 test_that("outputs a data.frame", {
-  expect_s3_class(prep_timeline(sda), "data.frame")
+  data <- head(sda)
+  expect_s3_class(prep_timeline(data), "data.frame")
 })
 
 test_that("with bad `extrapolate` errors gracefully", {
+  data <- head(sda)
   expect_error(
-    prep_timeline(sda, extrapolate = "bad"),
+    prep_timeline(data, extrapolate = "bad"),
     "logical.*not TRUE"
   )
 })
 
 test_that("outputs column `sector`", {
-  out <- prep_timeline(sda)
+  data <- head(sda)
+  out <- prep_timeline(data)
   expect_true(hasName(out, "sector"))
 })
 
 test_that("preserves sectors", {
-  out <- prep_timeline(sda)
+  data <- head(sda)
+  out <- prep_timeline(data)
   expect_equal(unique(out$sector), unique(out$sector))
 })
 
 test_that("outputs the expected snapshot", {
-  expect_snapshot(prep_timeline(sda))
+  data <- head(sda)
+  expect_snapshot(prep_timeline(data))
 })
 
 test_that("without crucial columns errors gracefully", {
-  expect_error(class = "missing_names", prep_timeline(select(sda, -sector)))
-  expect_error(class = "missing_names", prep_timeline(select(sda, -year)))
+  data <- head(sda)
+
+  expect_error(class = "missing_names", prep_timeline(select(data, -sector)))
+  expect_error(class = "missing_names", prep_timeline(select(data, -year)))
   expect_error(
     class = "missing_names",
-    prep_timeline(select(sda, -emission_factor_metric))
+    prep_timeline(select(data, -emission_factor_metric))
   )
   expect_error(
     class = "missing_names",
-    prep_timeline(select(sda, -emission_factor_value))
+    prep_timeline(select(data, -emission_factor_value))
   )
 })
 
 test_that("outputs expected colums", {
-  out <- prep_timeline(sda)
+  data <- head(sda)
+  out <- prep_timeline(data)
   expected <- c("year", "line_name", "value", "extrapolated")
   expect_true(all(expected %in% names(out)))
 })
