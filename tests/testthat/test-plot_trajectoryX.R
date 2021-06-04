5512608d36f78ca266f465183703a3045c388ec0
Mauro Lepore <mauro@2degrees-investing.org>
2021-06-04 03:16:27 -0600

    New plot_*X() with minimal arguments (#209)

---
+++ tests/testthat/test-plot_trajectoryX.R
@@ -0,0 +1,12 @@
+test_that("outputs a ggplot", {
+  data <- market_share %>%
+    filter(
+      sector == "power",
+      technology == "renewablescap",
+      region == "global",
+      scenario_source == "demo_2020"
+    )
+
+  p <- plot_trajectoryX(data, main = "projected", normalize = TRUE)
+  expect_s3_class(p, "ggplot")
+})
