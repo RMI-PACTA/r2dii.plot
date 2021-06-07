f2db0e16b1284044177b2a421be4f98c186562ea
Mauro Lepore <maurolepore@gmail.com>
2021-06-07 09:32:46 -0600

    Prune prep_techmixB()

--- R/prep_techmix.R
+++ R/prep_techmix.R
@@ -81,51 +81,6 @@ prep_techmix <- function(data,
   data_out
 }
 
-#' @rdname prep_techmix
-#' @export
-#' @examples
-#' library(dplyr)
-#'
-#' data <- market_share %>%
-#'   filter(
-#'     dplyr::between(year, 2020, 2025),
-#'     scenario_source == "demo_2020",
-#'     sector == "power",
-#'     region == "global",
-#'     metric %in% c("projected", "corporate_economy", "target_sds")
-#'   )
-#'
-#' prep <- prep_techmixB(data)
-prep_techmixB <- function(data, value = "technology_share", metric = "metric") {
-  check_prep_techmixB(data, value)
-
-  data %>%
-    recode_metric_and_metric_type(metric) %>%
-    pick_extreme_years() %>%
-    date_metric_type() %>%
-    mutate(value = .data[[value]])
-}
-
-pick_extreme_years <- function(data) {
-  filter(data, .data$year %in% c(min(data$year), max(data$year)))
-}
-
-date_metric_type <- function(data) {
-  mutate(data, metric_type = paste0(.data$metric_type, "_", .data$year))
-}
-
-check_prep_techmixB <- function(data, value) {
-  crucial <- c("metric", "year", "scenario_source", "region", value)
-  abort_if_missing_names(data, crucial)
-
-  cols <- c("scenario_source", "sector", "region")
-  lapply(cols, function(x) abort_multiple(data, x))
-
-  abort_bad_metric(data$metric)
-
-  invisible(data)
-}
-
 check_prep_techmix <- function(data,
                                years_filter,
                                region_filter,
