3ac9a2c9b60fe00818785a68b4a266f6caf10c20
Mauro Lepore <maurolepore@gmail.com>
2021-06-13 13:16:10 -0600

    Build temporary, brittle safety net

--- tests/testthat/test-plot_timelineX.R
+++ tests/testthat/test-plot_timelineX.R
@@ -3,3 +3,13 @@ test_that("outputs a ggplot", {
   p <- plot_timelineX(data)
   expect_s3_class(p, "ggplot")
 })
+
+test_that("outputs the expected ggplot object", {
+  mauro <- path.expand("~") == "/home/mauro"
+  skip_if_not(mauro, message = "Brittle test meant to run on mauro's pc only")
+
+  data <- filter(sda, sector == "cement")
+  p <- plot_timelineX(data)
+  p$plot_env <- NULL
+  expect_snapshot(str(p))
+})
