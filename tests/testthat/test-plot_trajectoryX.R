447f6639bd385a1e1fe521e7b7bfde6c3b4249be
Mauro Lepore <maurolepore@gmail.com>
2021-06-13 22:41:25 -0600

    Reorganize tests

--- tests/testthat/test-plot_trajectoryX.R
+++ tests/testthat/test-plot_trajectoryX.R
@@ -93,3 +93,144 @@ test_that("is sensitive to `main_line`", {
     )
   expect_no_error(plot_trajectoryX(data, main_line = "corporate_economy"))
 })
+
+# prep_trajectoryB() ----
+
+test_that("prep_trajectoryB() outputs the expected snapshot", {
+  data <- market_share %>%
+    filter(
+      technology == "oilcap",
+      region == "global",
+      scenario_source == "demo_2020",
+      year <= 2025,
+      sector == "power"
+    )
+  out <- prep_trajectoryB(data)
+
+  skip("Dead code")
+  expect_snapshot(out)
+})
+
+test_that("with multiple distinct values in some columns errors gracefully", {
+  long_sector <- mutate(head(market_share, 2), sector = 1:2)
+  skip("Dead code")
+  expect_snapshot_error(prep_trajectoryB(long_sector))
+
+  long_tech <- mutate(head(market_share, 2), technology = 1:2)
+  skip("Dead code")
+  expect_snapshot_error(prep_trajectoryB(long_tech))
+
+  long_region <- mutate(head(market_share, 2), region = 1:2)
+  skip("Dead code")
+  expect_snapshot_error(prep_trajectoryB(long_region))
+
+  long_source <- mutate(head(market_share, 2), scenario_source = 1:2)
+  skip("Dead code")
+  expect_snapshot_error(prep_trajectoryB(long_source))
+})
+
+test_that("if `normalize` isn't length-1 errors gracefully", {
+  skip("Dead code")
+  expect_snapshot_error(
+    prep_trajectoryB(market_share, normalize = c(TRUE, TRUE))
+  )
+})
+
+test_that("if `normalize` isn't logical errors gracefully", {
+  skip("Dead code")
+  expect_snapshot_error(
+    prep_trajectoryB(market_share, normalize = "a")
+  )
+})
+
+test_that("is sensitive to `normalize`", {
+  data <- market_share %>%
+    filter(
+      technology == "oilcap",
+      region == "global",
+      scenario_source == "demo_2020",
+      sector == "power"
+    )
+
+  expect_false(
+    identical(
+      prep_trajectoryB(data, normalize = TRUE),
+      prep_trajectoryB(data, normalize = FALSE)
+    )
+  )
+})
+
+test_that("with missing crucial names errors gracefully", {
+  data <- head(market_share)
+
+  bad <- select(data, -metric)
+  expect_error(class = "missing_names", prep_trajectoryB(bad))
+
+  bad <- select(data, -sector)
+  expect_error(class = "missing_names", prep_trajectoryB(bad))
+
+  bad <- select(data, -technology)
+  expect_error(class = "missing_names", prep_trajectoryB(bad))
+
+  bad <- select(data, -region)
+  expect_error(class = "missing_names", prep_trajectoryB(bad))
+
+  bad <- select(data, -year)
+  expect_error(class = "missing_names", prep_trajectoryB(bad))
+
+  bad <- select(data, -scenario_source)
+  expect_error(class = "missing_names", prep_trajectoryB(bad))
+
+  bad <- select(data, -production)
+  expect_error(class = "missing_names", prep_trajectoryB(bad))
+})
+
+test_that("integrates with plot_trajectory()", {
+  data <- market_share %>%
+    filter(
+      technology == "oilcap",
+      region == "global",
+      scenario_source == "demo_2020",
+      year <= 2025,
+      sector == "power"
+    )
+
+  out <- prep_trajectoryB(data)
+  expect_no_error(plot_trajectoryB(out))
+})
+
+# plot_trajectoryB() ----
+
+test_that("with wrong 'main_line' errors gracefully", {
+  data <- prep_trajectoryY(
+    market_share,
+    sector_filter = "power",
+    technology_filter = "oilcap",
+    region_filter = "global",
+    scenario_source_filter = "demo_2020",
+    value = "production",
+    end_year_filter = 2025,
+    normalize = TRUE
+  )
+  skip("Dead code")
+  expect_snapshot_error(
+    plot_trajectoryB(data, main_line = "bad")
+  )
+})
+
+test_that("with too long `main_line` errors gracefully", {
+  data <- prep_trajectoryY(
+    market_share,
+    sector_filter = "power",
+    technology_filter = "oilcap",
+    region_filter = "global",
+    scenario_source_filter = "demo_2020",
+    value = "production",
+    end_year_filter = 2025,
+    normalize = TRUE
+  )
+  skip("Dead code")
+  expect_snapshot_error(
+    plot_trajectoryB(data, main_line = c("too", "long"))
+  )
+})
