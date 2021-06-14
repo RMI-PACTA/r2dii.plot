3ba03c23d1195ca35e3066cdd1fd5d8362153270
Mauro Lepore <maurolepore@gmail.com>
2021-06-13 22:28:19 -0600

    Move code around

--- R/plot_trajectoryX.R
+++ R/plot_trajectoryX.R
@@ -333,3 +333,58 @@ assert_5_rows <- function(data) {
   stopifnot(nrow(data) == 5L)
   invisible(data)
 }
+
+#' @noRd
+#' @examples
+#' library(dplyr)
+#'
+#' data <- market_share %>%
+#'   filter(
+#'     technology == "oilcap",
+#'     region == "global",
+#'     scenario_source == "demo_2020",
+#'     year <= 2025,
+#'     sector == "power"
+#'   )
+#'
+#' prep_trajectoryB(data)
+prep_trajectoryB <- function(data,
+                             value = "production",
+                             metric = "metric",
+                             normalize = TRUE) {
+  check_prep_trajectoryB(data, value, normalize)
+  data <- recode_metric_and_metric_type(data, metric)
+
+  cols <- c("year", "metric_type", "metric", "technology", "value")
+  out <- data %>%
+    mutate(value = .data[[value]]) %>%
+    select(all_of(cols))
+
+  if (!normalize) {
+    return(out)
+  }
+
+  left_join(
+    out, filter(out, .data$year == min(.data$year)),
+    by = c("metric_type", "metric")
+  ) %>%
+    mutate(
+      value = .data$value.x / .data$value.y,
+      year = .data$year.x,
+      technology = .data$technology.x
+    ) %>%
+    select(all_of(cols))
+}
+
+check_prep_trajectoryB <- function(data, value, normalize) {
+  crucial <- c(common_crucial_market_share_columns(), value)
+  abort_if_missing_names(data, crucial)
+
+  abort_if_invalid_length(normalize)
+  stopifnot(is.logical(normalize))
+
+  cols <- c("sector", "technology", "region", "scenario_source")
+  lapply(cols, function(x) abort_if_multiple(data, x))
+
+  invisible(data)
+}
