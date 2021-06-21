b0b9c2ec4cf0eabdfa1f534f7e35430d6b5f928a
Mauro Lepore <maurolepore@gmail.com>
2021-06-21 15:44:41 -0600

    New utils2

---
+++ R/utils2.R
@@ -0,0 +1,235 @@
+beautify <- function(data, x) {
+  mutate(data, "{x}" := to_title(.data[[x]]))
+}
+
+#' Convert a string to title case
+#'
+#' This function replaces a sequence of non alpha-numeric characters to a single
+#' space, and applies title case to the remaining words.
+#'
+#' @param x A character vector.
+#'
+#' @return A character vector.
+#' @keywords internal
+#' @examples
+#' to_title(c("a.string", "ANOTHER_string"))
+#' to_title(c("a.string", "another_string", "b.STRING"))
+#' @noRd
+to_title <- function(x) {
+  to_title_one <- function(x) {
+    words <- tolower(unlist(strsplit(x, "[^[:alnum:]]+")))
+    # `toTitleCase()` with "a" returns "a", not "A" (a bug in this context)
+    words <- capitalize_single_letters(tools::toTitleCase(words))
+    paste(words, collapse = " ")
+  }
+
+  unlist(lapply(x, to_title_one))
+}
+
+capitalize_single_letters <- function(words) {
+  out <- words
+  out[which(nchar(out) == 1L)] <- toupper(out[which(nchar(out) == 1L)])
+  out
+}
+
+recode_metric <- function(x) {
+  case_when(
+    x == "projected" ~ "portfolio",
+    startsWith(x, "target") ~ "scenario",
+    TRUE ~ "benchmark"
+  )
+}
+
+abort_if_multiple <- function(data, x, env = parent.frame()) {
+  .data <- deparse_1(substitute(data, env = env))
+
+  do_it_once <- function(x) {
+    .x <- unique(data[[x]])
+    if (length(.x) > 1L) {
+      abort(c(
+        glue("`{.data}` must have a single value of `{x}`."),
+        x = glue("Provided: {toString(.x)}."),
+        i = glue("Pick one value, e.g. '{first(.x)}', with:
+          subset({.data}, {x} == '{first(.x)}')")
+      ))
+    }
+    invisible(x)
+  }
+  lapply(x, do_it_once)
+
+  invisible(data)
+}
+
+# Backport `base::deparse1()` to R < 4.0.0
+deparse_1 <- function(expr, collapse = " ", width.cutoff = 500L, ...) {
+  paste(deparse(expr, width.cutoff, ...), collapse = collapse)
+}
+
+abort_if_has_zero_rows <- function(data, env = parent.frame()) {
+  .data <- deparse_1(substitute(data, env = env))
+  if (nrow(data) == 0L) {
+    abort(c(glue("`{.data}` must have some rows.", x = "It has none.")))
+  }
+
+  invisible(data)
+}
+
+hint_if_missing_names <- function(expr, like) {
+  rlang::with_handlers(
+    expr,
+    missing_names = function(err) {
+      abort(
+        c(conditionMessage(err), i = glue("Is your data `{like}`-like?")),
+        class = "hint_missing_names",
+        parent = err
+      )
+    }
+  )
+}
+
+common_crucial_market_share_columns <- function() {
+  c(
+    "metric",
+    "region",
+    "scenario_source",
+    "sector",
+    "technology",
+    "year"
+  )
+}
+
+#' Check if a named object contains expected names
+#'
+#' Based on fgeo.tool::abort_if_missing_names()
+#'
+#' @param x A named object.
+#' @param expected_names String; expected names of `x`.
+#'
+#' @return Invisible `x`, or an error with informative message.
+#'
+#' @examples
+#' x <- c(a = 1)
+#' abort_if_missing_names(x, "a")
+#' try(abort_if_missing_names(x, "bad"))
+#' @noRd
+abort_if_missing_names <- function(data, expected_names) {
+  stopifnot(rlang::is_named(data))
+  stopifnot(is.character(expected_names))
+
+  if (!all(unique(expected_names) %in% names(data))) {
+    missing_names <- sort(setdiff(expected_names, names(data)))
+    abort(c(
+      "`data` must have all the expected names.",
+      x = glue("Missing names: {toString(missing_names)}.")
+    ),
+    class = "missing_names"
+    )
+  }
+
+  invisible(data)
+}
+
+fmt_string <- function(x) {
+  toString(paste0("'", x, "'"))
+}
+
+fmt_vector <- function(x) {
+  paste0("c(", x, ")")
+}
+
+expect_no_error <- function(...) {
+  testthat::expect_error(..., NA)
+}
+
+expect_no_message <- function(...) {
+  testthat::expect_message(..., NA)
+}
+
+example_market_share <- function(...) {
+  filter(market_share, .data$technology == first(.data$technology), ...)
+}
+
+r_version_is_older_than <- function(major) {
+  as.integer(R.version$major) < major
+}
+
+#' The metric to plot most saliently
+#'
+#' The concept of "main line" is not obvious from the literal "projected" and
+#' we reuse it in multiple places, so it seems worth to capture the concept
+#' in this function's name.
+#' @examples
+#' main_line()
+#' @noRd
+main_line <- function() "projected"
+
+#' r2dii.plot options
+#'
+#' * `r2dii.plot.quiet`: `TRUE` suppresses user-facing messages.
+#'
+#' @noRd
+quiet <- function() getOption("r2dii.plot.quiet") %||% FALSE
+
+get_common_start_year <- function(data) {
+  data %>%
+    group_by(.data[[metric(data)]]) %>%
+    summarise(year = min(.data$year)) %>%
+    pull(.data$year) %>%
+    max()
+}
+
+#' The name of the column holding metrics such as projected, corporate_economy
+#'
+#' @examples
+#' metric(sda)
+#' metric(market_share)
+#' @noRd
+metric <- function(data) {
+  extract_names(data, metric_names())
+}
+
+#' Names of columns holding metrics such as projected, corporate_economy
+#'
+#' The column holding metrics such as "projected" and "corporate_economy" may
+#' have a different name in different datasets. This function outputs all the
+#' possible names. Eventually the difference may disappear (r2dii.analysis#313)
+#' and this function should help make the transition smooth.
+#'
+#' @examples
+#' metric_names()
+#' @noRd
+metric_names <- function() c("metric", "emission_factor_metric")
+
+#' Extract names matching `possible_names`
+#'
+#' @examples
+#' extract_names(sda, metric_names())
+#' extract_names(market_share, metric_names())
+#' extract_names(mtcars, c("mpg", "bad", "disp"))
+#' @noRd
+extract_names <- function(data, possible_names) {
+  doit_once <- function(x) grep(x, names(data), value = TRUE)
+
+  x <- anchor(possible_names)
+  unlist(lapply(x, doit_once))
+}
+
+anchor <- function(x) paste0("^", x, "$")
+
+drop_rows_before_sart_year <- function(data, metric) {
+  start_year <- get_common_start_year(data)
+  if (!min(data$year) < start_year) {
+    return(data)
+  }
+
+  if (!quiet()) {
+    inform(glue(
+      "Removing data before {start_year} -- the start year of 'projected'."
+    ))
+  }
+  filter(data, .data$year >= start_year)
+}
+
+is_scenario <- function(x) grepl("^target", x, ignore.case = TRUE)
+is_portfolio <- function(x) grepl("^projected", x, ignore.case = TRUE)
+is_benchmark <- function(x) !grepl("^projected|^target", x, ignore.case = TRUE)
