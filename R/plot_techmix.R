1d5b4101e5d5f1632ed4773e49bfea3e973680e9
Mauro Lepore <maurolepore@gmail.com>
2021-06-22 16:52:32 -0600

    Refactor

--- R/plot_techmix.R
+++ R/plot_techmix.R
@@ -49,8 +49,8 @@ check_plot_techmix <- function(data, env = parent.frame()) {
 abort_if_multiple_scenarios <- function(data, env = parent.frame()) {
   .data <- deparse_1(substitute(data, env = env))
 
-  scen <- extract_scenarios(data$metric)
-  n <- length(scen)
+  scenarios <- extract_scenarios(data$metric)
+  n <- length(scenarios)
 
   if (n == 0L) {
     abort(c(
@@ -60,14 +60,14 @@ abort_if_multiple_scenarios <- function(data, env = parent.frame()) {
   }
 
   if (n > 1L) {
-    example <- c(setdiff(unique(data$metric), scen), first(scen))
+    example <- c(setdiff(unique(data$metric), scenarios), first(scenarios))
     abort(c(
       glue("`{.data}$metric` must have a single scenario not {n}."),
       i = glue(
-        "Do you need to pick one scenario? E.g. pick '{first(scen)}' with: \\
+        "Do you need to pick one scenario? E.g. pick '{scenarios[[1]]}' with: \\
         `subset({.data}, metric %in% {fmt_vector(fmt_string(example))})`."
       ),
-      x = glue("Provided: {toString(scen)}.")
+      x = glue("Provided: {toString(scenarios)}.")
     ))
   }
 
@@ -78,16 +78,15 @@ prep_techmix <- function(data) {
   data %>%
     common_prep() %>%
     filter(.data$year %in% c(min(.data$year), max(.data$year))) %>%
+    # TODO: Should we make this in qplot_()?
     mutate(
       label = recode_metric(.data$label),
-      label = paste0(.data$label, "_", .data$year),
       label = to_title(.data$label),
       label = sub("target_", "", .data$label)
     ) %>%
-    mutate(
-      value = .data$technology_share,
-      sector = recode_sector(.data$sector)
-    )
+    # TODO: Should we instead facet by year?
+    mutate(label = paste0(.data$label, "_", .data$year)) %>%
+    mutate(value = .data$technology_share, sector = recode_sector(.data$sector))
 }
 
 plot_techmix_impl <- function(data) {
