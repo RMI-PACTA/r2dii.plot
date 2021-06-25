e2a6b6bfe92fba711bd1b0dbd3c7aaa617100db0
MonikaFu <monika.furdyna@gmail.com>
2021-06-25 19:28:01 +0200

    Add tests

---
+++ tests/testthat/_snaps/qplot_emission_intensity.md
@@ -0,0 +1,12 @@
+# Prints title as expected
+
+    Emission intensity trend for Automotive
+
+# Prints axis labels as expected
+
+    [1] "Year"
+
+---
+
+    [1] "Tons of CO2 per ton"
+
--- tests/testthat/test-qplot_emission_intensity.R
+++ tests/testthat/test-qplot_emission_intensity.R
@@ -6,3 +6,28 @@ test_that("outputs pretty labels", {
   pretty <- c("Projected", "Corporate Economy")
   expect_equal(pretty, metrics)
 })
+
+test_that("Prints title as expected", {
+  data <- filter(sda, sector == "automotive")
+  p <- qplot_emission_intensity(data)
+
+  expect_snapshot_output(p$labels$title)
+})
+
+test_that("Prints axis labels as expected", {
+  data <- filter(sda, sector == "automotive")
+  p <- qplot_emission_intensity(data)
+
+  expect_equal(p$labels$x, "Year")
+  expect_snapshot_output(p$labels$x)
+
+  expect_match(p$labels$y, "[Tt]ons of CO2 per ton")
+  expect_snapshot_output(p$labels$y)
+})
+
+test_that("Plots a data set with maximum time horizon of 5 years", {
+  data <- filter(sda, sector == "automotive")
+  p <- qplot_emission_intensity(data)
+
+  expect_true(max(p$data$year) - min(p$data$year) <= 5)
+})
