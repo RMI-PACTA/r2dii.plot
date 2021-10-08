b9297219b9a983924f73af5c2eef2973f7f23dff
Mauro Lepore <maurolepore@gmail.com>
2021-10-08 15:05:11 -0600

    Remove temporary tests

--- tests/testthat/test-utils.R
+++
@@ -1,9 +0,0 @@
-test_that("abort_if_unknown_values() works as expected", {
-  my_data <- tibble(sector = c("power", "automotive"))
-  expect_error(
-    abort_if_unknown_values("bad", my_data, "sector"), class = "unknown_value"
-  )
-  expect_error(abort_if_unknown_values("bad", my_data, "sector"), "bad")
-  bad <- c("val1", "val2")
-  expect_error(abort_if_unknown_values(bad, my_data, "sector"), "val2")
-})
