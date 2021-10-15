5d07ed1ce40ef1f1e6018daec8d2114a50e66bc4
Mauro Lepore <maurolepore@gmail.com>
2021-10-15 12:02:33 -0600

    Exclude tribbles from styler

--- tests/testthat/test-to_title.R
+++ tests/testthat/test-to_title.R
@@ -1,15 +1,18 @@
 test_that("to_title() converts labels correctly", {
+  # styler: off
   data <- tribble(
               ~input,          ~output,
           "a.string",       "A String",
     "another_string", "Another String",
           "b.STRING",       "B String"
   )
+  # styler: on
   helper <- to_title
   expect_equal(helper(data$input), data$output)
 })
 
 test_that("format_metric() converts labels correctly", {
+  # styler: off
   data <- tribble(
                ~input,             ~output,
           "projected",         "Projected",
@@ -18,11 +21,13 @@ test_that("format_metric() converts labels correctly", {
          "target_sds",               "SDS",
          "target_sps",               "SPS"
   )
+  # styler: on
   helper <- format_metric
   expect_equal(helper(data$input), data$output)
 })
 
 test_that("recode_metric_techmix() converts labels correctly", {
+  # styler: off
   data <- tribble(
            ~input,     ~output,
       "projected", "Portfolio",
@@ -33,11 +38,13 @@ test_that("recode_metric_techmix() converts labels correctly", {
        ".target_", "Benchmark",
             "xyz", "Benchmark"
   )
+  # styler: on
   helper <- recode_metric_techmix
   expect_equal(helper(data$input), data$output)
 })
 
 test_that("spell_out_technology() converts techmix labels correctly", {
+  # styler: off
   data <- tribble(
              ~input,                   ~output,
               "gas",                     "Gas",
@@ -45,6 +52,7 @@ test_that("spell_out_technology() converts techmix labels correctly", {
           "coalcap",           "Coal Capacity",
           "the_hdv", "The Heavy Duty Vehicles"
   )
+  # styler: on
   helper <- spell_out_technology
   expect_equal(helper(data$input), data$output)
 })
