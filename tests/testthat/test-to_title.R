114a2853c830883af540fc3dbf7bc69ffca32568
Mauro Lepore <maurolepore@gmail.com>
2021-10-15 09:07:15 -0600

    Fix expectation

--- tests/testthat/test-to_title.R
+++ tests/testthat/test-to_title.R
@@ -1,10 +1,10 @@
 test_that("spell_out_technology() converts techmix labels", {
   data <- tibble::tribble(
-        ~technology,             ~expected,
-              "gas",                 "Gas",
-              "ice",                 "ICE",
-          "coalcap",       "Coal Capacity",
-          "xyz_hdv", "Heavy Duty Vehicles",
+        ~technology,                 ~expected,
+              "gas",                     "Gas",
+              "ice",                     "ICE",
+          "coalcap",           "Coal Capacity",
+          "xyz_hdv", "Xyz Heavy Duty Vehicles",
 
   )
   expect_equal(spell_out_technology(data$technology), data$expected)
