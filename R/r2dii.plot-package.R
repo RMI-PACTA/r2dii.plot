9d680870d8d14171d18a7c68cfe1814fd6e136dc
MonikaFu <monika.furdyna@gmail.com>
2022-04-29 15:23:14 +0200

    Add dependency

--- R/r2dii.plot-package.R
+++ R/r2dii.plot-package.R
@@ -5,6 +5,7 @@
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 #' @importFrom dplyr n
+#' @importFrom lifecycle deprecate_soft
 #' @importFrom scales percent
 #' @importFrom stringr str_count
 ## usethis namespace: end
